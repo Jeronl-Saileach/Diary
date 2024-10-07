38950afbfda1305a8db7738cf4a9df3ebf31d5ca
zhiyirrrd <113674772+zhiyirrrd@users.noreply.github.com>
2024-10-07 20:31:24 +0800

    Add files via upload

---
+++ main/java/com/example/fruitapplication/DiaryEntryActivity.java
@@ -0,0 +1,28 @@
+package com.example.fruitapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DiaryEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_entry);
+        Button returnToMain = findViewById(R.id.returnToMain);
+
+        // 处理 Return 按钮点击
+        returnToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DiaryEntryActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        // Implement rich text editing and multimedia insert here later
+    }
+}
