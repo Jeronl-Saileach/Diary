6af3d9bd4884e37e938bb898b4e6db6c602684fd
zhiyirrrd <113674772+zhiyirrrd@users.noreply.github.com>
2024-10-08 20:19:36 +0800

    Add files via upload
    版本更新至1.1
    本次更新内容为
    1.添加了部分背景图，增加美观度
    2.填充了基本框架
    3.增加了应用图标（暂时不知道为什么显示的是马赛克）

---
+++ main/java/com/example/fruitapplication/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.fruitapplication; // 请确保包名和您的实际包名一致
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login); // 确保此布局文件存在
+
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // 关闭当前活动
+            }
+        });
+    }
+}
